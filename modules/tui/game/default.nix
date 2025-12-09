88acb948a9817d37897211d6096c5139727f4bb4
HadziqM <dimascrazz@gmail.com>
2025-12-09 12:16:21 +0700

    add gamescope args

--- modules/tui/game/default.nix
+++ modules/tui/game/default.nix
@@ -3,8 +3,8 @@ let
   vn-fix = writeScriptBin "vn-fix.sh" (builtins.readFile ./vn.sh);
   jadeite = pkgs.callPackage ./jadeite.nix { };
 
-  # from protonup-rs, proton cant be declarative because valve license policy
-  # i can make it myself if i didnt upload the code to github
+  # from protonup-rs, proton cant be packaged because valve license policy
+  # i can make it myself if i didnt need to upload the code to github
   proton-path = "$HOME/.steam/steam/compatibilitytools.d/GE-Proton10-26";
   # TODO: make script to automate making prefix with available fix
   prefix = "$HOME/temp/prefix";
@@ -13,23 +13,20 @@ let
     if [ "$WINEPREFIX" = "" ]; then WINEPREFIX=${prefix}; fi
 
     export WINEDEBUG="-all"
+    export WINEDLLOVERRIDES="d3d11=n;d3d12=n;dxgi=n"
     export WINEPREFIX
     export PROTONPATH
 
     echo "using prefix $WINEPREFIX"
     echo "using proton $PROTONPATH"
   '';
-
-  game-run = writeScriptBin "game-run.sh" ''
-    ${set-env}
-
-    exec ${pkgs.gamemode}/bin/gamemoderun \
-         ${pkgs.gamescope}/bin/gamescope -- \
-         ${pkgs.umu-launcher}/bin/umu-run "$@"
+  mango-config = ''
+    export MANGOHUD_CONFIG="font_size=12,preset=3"
   '';
-
+  gamescope-arg = "-w 1920 -h 1080 -f --force-grab-cursor --adaptive-sync";
   gacha-run = writeScriptBin "gacha-run.sh" ''
     ${set-env}
+    ${mango-config}
     LINUX_PATH="$1"
 
     # convert relative to absolute path
@@ -37,16 +34,33 @@ let
       LINUX_PATH="$(pwd)/$LINUX_PATH"
     fi
 
-    # wine always treat linux abs path with z:
+    # wine always treat linux root folder to z: disk
     WIN_PATH="$(printf '%s' "Z:$LINUX_PATH" | sed 's|/|\\|g')"
 
     echo "using win path $WIN_PATH"
 
     exec ${pkgs.gamemode}/bin/gamemoderun \
-       ${pkgs.gamescope}/bin/gamescope -- \
-       ${pkgs.umu-launcher}/bin/umu-run \
-       ${jadeite}/share/jadeite/jadeite.exe "$WIN_PATH"
+         ${pkgs.gamescope}/bin/gamescope ${gamescope-arg} -- \
+         ${pkgs.umu-launcher}/bin/umu-run \
+         ${pkgs.mangohud}/bin/mangohud \
+         ${jadeite}/share/jadeite/jadeite.exe "$WIN_PATH"
   '';
+  game-run = writeScriptBin "game-run.sh" ''
+    ${set-env}
+
+    exec ${pkgs.gamemode}/bin/gamemoderun \
+         ${pkgs.gamescope}/bin/gamescope ${gamescope-arg} -- \
+         ${pkgs.umu-launcher}/bin/umu-run "$@"
+  '';
+  game-run-mango = writeScriptBin "game-mango.sh" ''
+    ${set-env}
+    ${mango-config}
+    exec ${pkgs.gamemode}/bin/gamemoderun \
+         ${pkgs.gamescope}/bin/gamescope ${gamescope-arg} -- \
+         ${pkgs.mangohud}/bin/mangohud \
+         ${pkgs.umu-launcher}/bin/umu-run "$@"
+  '';
+
 in
 pkgs.symlinkJoin {
   name = "game";
@@ -55,6 +69,7 @@ pkgs.symlinkJoin {
     vn-fix
     game-run
     gacha-run
+    game-run-mango
     umu-launcher
     wineWowPackages.stable
     protonup-rs
